890dce98039626d258524795bfabb1395edd7287
SerhiiMoseichuk <sergiymoseichuk@gmail.com>
2021-12-14 18:35:57 +0200

    version 2.0 without rand method

--- src/com/epam/learning/lecture1/MoreOrLess.java
+++ src/com/epam/learning/lecture1/MoreOrLess.java
@@ -1,24 +1,39 @@
 package com.epam.learning.lecture1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MoreOrLess {
 
+//    int rand (int min, int max){
+//        int value = 1 + (int) (Math.random() * (max - min));
+//        return value;
+//    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         int randomVal = 1 + (int) (Math.random() * 100);
+
         int number = -1;
         int minValue = 0;
         int maxValue = 100;
         String result = "";
+
+        Scanner sc = new Scanner(System.in);
         do {
             System.out.print("Try to guess: ");
-            number = sc.nextInt();
-            if (number == 123) {
+            try { // перевірка на введення "не числа"
+                number = sc.nextInt();
+            } catch (InputMismatchException ex) {
+                throw new InputMismatchException("Input NaN");
+            }
+            if (number > 100 || number < 0) throw new NumberFormatException("Out of range [0,100]"); // перевірка відповідності введеного числа діапазону
+
+            if (number == 123) {  // вихід з програми при введені "123"
                 System.out.println("You surrendered :(");
                 break;
             } else {
-                result += number + " ";
+                result += number + " "; // запис спроб користувача
                 if (randomVal > number) {
                     System.out.println("more...");
                     minValue = number;
@@ -32,10 +47,8 @@ public class MoreOrLess {
             }
         } while (number != randomVal);
         System.out.println("Your attempts - [ " + result + "]");
-
+        sc.close();
     }
 
-    int rand (int min, int max){
-        return 0;
-    }
+
 }
